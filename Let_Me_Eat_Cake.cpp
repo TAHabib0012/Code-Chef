da2629ad69d47763ada6aed493217f0317ebe22f
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-09-05 21:02:38 +0600

    Solved

---
+++ Let_Me_Eat_Cake.cpp
@@ -0,0 +1,60 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    int n, m, c = 0;
+    cin >> n >> m;
+
+    while (n != m)
+    {
+        if (n > m)
+        {
+            int x = (n + 1) / 2; 
+            n =  n - x;      
+            c = c + x;              
+        }
+        else
+        {
+            int x = (m + 1) / 2;
+            m = m - x;
+            c = c + x;            
+        }
+    }
+    cout << c << endl;
+}
+
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}

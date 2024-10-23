5b2cd272474a78a9e4d6f3b39525f837d8ede485
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-10-24 02:25:49 +0600

    Solved

---
+++ Non_matching_Name.cpp
@@ -0,0 +1,134 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    int n, m;
+    cin >> n >> m;
+
+    string s, t;
+    cin >> s >> t;
+
+    map<char,int> mp;
+
+    for (int i = 0; i < n; i++)
+    {
+        mp[s[i]]++;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        mp[t[i]]++;
+    }
+    
+    for (char i = 'a'; i <= 'z'; i++)
+    {
+        if (mp[i] == 0)
+        {
+            yes;
+            return;
+        }
+        
+    }
+    
+    no;
+
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
+
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    LL n, c = 1, f = 0;
+    cin >> n;
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        if (i < n && a[i] == a[i + 1])
+        {
+            c++;
+        }
+        else
+        {
+            f = f + (c * (c + 1)) / 2;
+            c = 1;
+        }
+    }
+    
+    cout << f << endl;
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}

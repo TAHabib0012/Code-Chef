5b8f362bbb08c3c68446d6ffe5dbf6fdf5d5c5f4
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-12-25 20:54:47 +0600

    Solved

---
+++ Chefland_Library.cpp
@@ -0,0 +1,57 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    LL n, s = 0;
+    cin >> n;
+    vector<LL> a(n);
+    unordered_map<LL, LL> mp;
+
+    for (LL i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        mp[a[i]] = max(mp[a[i]], i + 1);
+    }
+
+    for (auto i : mp)
+    {
+        s = s + i.second;
+    }
+
+    cout << s << endl;
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
